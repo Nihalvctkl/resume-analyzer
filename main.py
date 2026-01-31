da79cea32c63e353bc84a71f94b1c453bd52f8ba
Nihalvctkl <nihalvctkl@gmail.com>
2026-01-31 22:06:31 +0530

    Add application entry point and basic module imports

---
+++ main.py
@@ -0,0 +1,8 @@
+from parser.pdf_parser import parse_pdf
+from analyzer.skill_extractor import extract_skills
+
+def main():
+    print("Resume Analyzer started")
+
+if __name__ == "__main__":
+    main()
